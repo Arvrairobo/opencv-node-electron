402fe47147d6ea9d249c166412a29d6e1a8a32d3
Jérémy Morin <hi@jermor.in>
2016-02-21 18:44:00 +0100

    feat(webcam) improve with eye detection

--- readme.md
+++ readme.md
@@ -8,6 +8,12 @@
 [![bitHound Dev Dependencies](https://www.bithound.io/github/Jermorin/opencv-node-electron/badges/devDependencies.svg)](https://www.bithound.io/github/Jermorin/opencv-node-electron/master/dependencies/npm)
 [![bitHound Code](https://www.bithound.io/github/Jermorin/opencv-node-electron/badges/code.svg)](https://www.bithound.io/github/Jermorin/opencv-node-electron)
 
+##install
+
+- install opencv 2.4.*
+- `npm install`
+- `npm start`
+
 ##packages
 
 - [electron](http://electron.atom.io/)
--- src/socket.js
+++ src/socket.js
@@ -1,14 +1,16 @@
 var cv = require('opencv');
 
 // camera properties
-var camWidth = 700;
-var camHeight = 500;
+var camWidth = 500;
+var camHeight = 300;
 var camFps = 10;
 var camInterval = 1000 / camFps;
 
 // face detection properties
-var rectColor = [0, 255, 0];
-var rectThickness = 2;
+var rectColorFace = [0, 255, 0];
+var rectColorEyes = [0, 0, 255];
+var rectThicknessFace = 2;
+var rectThicknessEyes = 1;
 
 // initialize camera
 var camera = new cv.VideoCapture(0);
@@ -16,21 +18,37 @@ var camera = new cv.VideoCapture(0);
 camera.setWidth(camWidth);
 camera.setHeight(camHeight);
 
+function detectFace(im, cb) {
+    im.detectObject('./node_modules/opencv/data/haarcascade_frontalface_alt.xml', {}, function (err, faces) {
+        if (err) throw err;
+        for (var i = 0; i < faces.length; i++) {
+            face = faces[i];
+            im.rectangle([face.x, face.y], [face.width, face.height], rectColorFace, rectThicknessFace);
+        }
+        cb(im);
+    });
+}
+
+function detectEyes(im, cb) {
+    im.detectObject('./node_modules/opencv/data/haarcascade_eye.xml', {}, function (err, eyes) {
+        if (err) throw err;
+        for (var i = 0; i < eyes.length; i++) {
+            eye = eyes[i];
+            im.rectangle([eye.x, eye.y], [eye.width, eye.height], rectColorEyes, rectThicknessEyes);
+        }
+        cb(im);
+    });
+}
+
+
 module.exports = function (socket) {
     setInterval(function () {
         camera.read(function (err, im) {
             if (err) throw err;
-
-            //you can detect many things go to ./node_modules/opencv/data/
-            im.detectObject('./node_modules/opencv/data/haarcascade_frontalface_alt.xml', {}, function (err, faces) {
-                if (err) throw err;
-
-                for (var i = 0; i < faces.length; i++) {
-                    face = faces[i];
-                    im.rectangle([face.x, face.y], [face.width, face.height], rectColor, rectThickness);
-                }
-
-                socket.emit('frame', {buffer: im.toBuffer()});
+            detectFace(im, function (imWithFace) {
+                detectEyes(imWithFace, function (imFinal) {
+                    socket.emit('frame', {buffer: imFinal.toBuffer()});
+                });
             });
         });
     }, camInterval);
